64be6a8e9924ae4a1e0e65a4fae354366b8657fa
Jack Dwyer <jackjack.dwyer@gmail.com>
2016-09-18 22:33:11 -0400

    supporting piwiriing

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
-.PHONY: upload console setup deploy
+.PHONY: upload console setup deploy controller
 
 upload:
-	scp nunchuck_reader.c controller.py $(RPI_USER)@$(RPI_HOST):~/rc-car/
+	scp Makefile controller.c nunchuck_reader.c controller.py $(RPI_USER)@$(RPI_HOST):~/rc-car/
 
 console:
 	ssh $(RPI_USER)@$(RPI_HOST)
 
+controller:
+	gcc -Wall -o controller controller.c -lwiringPi
+
+nunchuck:
+	gcc -Wall -lwiringPi -o nunchuck_reader nunchuck_reader.c 
+
+deploy: upload
+
 setup:
 	scp setup.sh $(RPI_USER)@$(RPI_HOST):~/setup.sh
-	ssh $(RPI_USER)@$(RPI_HOST) /home/$(RPI_USER)/setup.sh
-	@ echo "Setup Complete"
-	@ echo "----------------------------------"
-
-deploy: setup upload
+	# ssh $(RPI_USER)@$(RPI_HOST) /home/$(RPI_USER)/setup.sh
+	# @ echo "Setup Complete"
+	# @ echo "----------------------------------"
--- setup.sh
+++ setup.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-
-echo "[000] Setup Called"
+echo "[000/007] Setup Called"
 echo "----------------------------------"
-echo "[001] Creating $HOME/rc-car"
+
+echo "[001/007] Updating apt"
+sudo apt-get update -y
+echo "----------------------------------"
+
+echo "[002/007] installing git"
+sudo apt-get install -y git
+echo "----------------------------------"
+
+echo "[003/007] Creating $HOME/rc-car"
 [[ -d $HOME/rc-car ]] || mkdir $HOME/rc-car
 echo "----------------------------------"
-echo "[002] installing python-pip gcc"
+
+echo "[004/007] installing python-pip gcc"
 sudo apt-get install -y python-pip gcc 
 echo "----------------------------------"
-echo "[003] pip install readchar"
+
+echo "[005/007] pip install readchar"
 sudo pip install readchar
 echo "----------------------------------"
+
+echo "[006/007] Cloning wiringPi"
+git clone git://git.drogon.net/wiringPi
+echo "----------------------------------"
+
+echo "[007/007] Building wiringPi"
+cd wiringPi; ./build
+echo "----------------------------------"
+
 echo "Done!"
